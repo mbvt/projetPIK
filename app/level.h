58e91907eb9b7f3e81e3e903f9b57801d37a15cf
bquinne <brandonquinne@hotmail.fr>
2017-02-13 21:46:20 +0100

    adding prototype for level.c TODO: finish it

--- app/level.h
+++ app/level.h
@@ -10,4 +10,14 @@ struct level {
   int difficulty;
   int category;
 };
+/*
+**  Init a level with the world (3) , stage(3), difficulty(3), category (atm 4)
+*/
+struct level *init_level (int world, int stage, int difficulty, int category,
+    int id_statistic);
+
+/*
+**  get the level with the max of idNIV done = 0 user_id
+*/
+struct level *actual_level (int user_id )
 #endif
