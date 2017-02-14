c5510304a4326dd296c16b1a432b3011b7bfa745
Brandon Quinne <brandon.quinne@epita.fr>
2017-02-14 19:29:20 +0100

    dev functions TODO: include query

--- app/profile.c
+++ app/profile.c
@@ -3,75 +3,48 @@
 #include <stdio.h>
 #include "profile.h"
 #include "dico.h"
-struct profile *init_profile(int id, char *name, struct dico *dico)
+struct profile *init_profile(int id_category, struct user *user)
 {
-    struct profile *profile = malloc(sizeof (struct profile));
-    profile->id = id;
-    profile->name = name;
-    profile->dico = dico; 
-    return profile;
+  struct profile *profile = malloc(sizeof (struct profile));
+  profile->id_catefory = id_category;
+  profile->user = user;
+  return profile;
 }
 
-struct profile *determine_profile(int age, int key, int handicap)
+struct profile *determine_category(int status, int handicap)
 {
-// enfant
-// handicap
-// black_white
-// visual
-// motor
-// noob
-// normal
-// expert$
+  // enfant
+  // handicap
+  // black_white
+  // visual
+  // motor
+  // noob
+  // normal
+  // expert$
 
 
-
-/* TODO determiner 
+/* TODO determiner
 ** les dicos
 ** les speed
-** les clavier couleur etc 
+** les clavier couleur etc
 ** est il possible d'avoir plussieur handicap + etre enfant
 */
-    int     id;
-    char    *name;
-    int     key;    
-    int speed;
-    if (age < 12)
-    {// dico enfant
-        id = 1;
-        name = "child";
-        dico = 2 ;
-    }
-    if (handicap == 1)
-    {
-        id = 2;
-        name = "black_white";
-        key = 1;//keyboard black and white
-    }else if (handicap == 2)
-    {
-        id = 3;
-        name = "visual"
-        key = 2; // keyboard loupe
-        
-    }else if (handicap == 3)
-    {
-        id = 4;
-        name = "motor";
-        speed = 1; // modifier la rapidité
-    }
-    if (key == 0)
-    {
-        id = 5 ;
-        name = "noob";
-        dico = 2; // dico normale
-        speed = 1;
-    }else
-    {
-        id = 6;
-        name = "normal";
-        dico = 2; // dico normale
-        speed = 1;
-    }
-       
-    struct profile *profile = init_profile(id,name,dico);
-    return profile;
+
+  int category;
+
+/*
+** TODO category = status ??
+*/
+  if (status > -1)
+    category = status
+  else
+    return -1;
+
+  if (handicap > 0)
+    category = handicap +1;
+    //verify that category exist in database
+  else
+    return -1
+
+  return category;
 }
--- app/user.c
+++ app/user.c
@@ -4,82 +4,96 @@
 # include <string.h>
 # include "user.h"
 
-struct user *init_user(char *name, char *firstname, int age,
-            struct profile *profile){
-    
-    struct user *newuser =  malloc(sizeof (struct user));
-    newuser->name =            name;  
-    newuser->firstname =       firstname;  
-    newuser->age =             age;  
-    newuser->profile =         profile;
+struct user *init_user(char *name, char *firstname, int age, int status, int
+    handicap, struct profile *profile){
 
-    char profname[] = *(profile->name);
-    
-    char append[2];
-    FILE *file = fopen("~/data/user","w");
-    char *line = malloc(256*sizeof(char));
-    strcat(line,name);
-    strcat(line,"/");
-    strcat(line,firstname);
-    strcat(line,"/");
-    sprintf(append,"%d",age);
-    strcat(line,append);
-    strcat(line,"/");
-    strcat(line,profname);
-    fwrite(line,1,sizeof(line),file);
-    fclose(file);
+  struct user *newuser =  malloc(sizeof (struct user));
+  newuser->name =            name;
+  newuser->firstname =       firstname;
+  newuser->age =             age;
+  newuser->profile =         profile;
 
-    return  newuser;
+  //char profname[] = *(profile->name);
+
+  //TODO Remplace next lines with Database query
+  char append[2];
+  FILE *file = fopen("~/data/user","w");
+  char *line = malloc(256*sizeof(char));
+  strcat(line,name);
+  strcat(line,"/");
+  strcat(line,firstname);
+  strcat(line,"/");
+  sprintf(append,"%d",age);
+  strcat(line,append);
+  strcat(line,"/");
+  strcat(line,profname);
+  fwrite(line,1,sizeof(line),file);
+  fclose(file);
+
+  return  newuser;
 }
 
-struct user *find_user(char *name, char *firstname){
-    FILE *file = fopen("~/data/user","r");
-    struct user *user = malloc(sizeof (struct user));
-    char line[128];
-    int i = 0;
-    int find = 0;
-    while(fgets(line, sizeof (line), file) != NULL && find == 0 )
-    {
-        //for each elmt seperate by delimiter
-        // stocker dans liste tout la ligne
-        // si l.name = name && l.firstname = firstname
-        // init_user avec les elmt de la liste retourné le user.
-        printf("line %d : %s",i,line);
-        i++;
-    }
+int find_user(char *name, char *firstname, struct user **user){
 
-    fclose(file);
-    return user; 
+  //TODO Query select user
+  // then initialize a user
+  FILE *file = fopen("~/data/user","r");
+  struct user *user = malloc(sizeof (struct user));
+  char line[128];
+  int i = 0;
+  int find = 0;
+  while(fgets(line, sizeof (line), file) != NULL && find == 0 )
+  {
+    //for each elmt seperate by delimiter
+    // stocker dans liste tout la ligne
+    // si l.name = name && l.firstname = firstname
+    // init_user avec les elmt de la liste retourné le user.
+    printf("line %d : %s",i,line);
+    i++;
+  }
+
+  fclose(file);
+  return user;
 }
 
-struct user *mcq_user(void)
+int *mcq_user(struct user **user)
 {
-    struct user *user;
-    struct profile *profile;
-    char name[50];
-    char firstname[50];
-    char age[3];
-    int keyboard;
-    int handicap;
-    
-    printf("Entrez votre nom:\n");
-    scanf("%s",name);
-    printf("Entrez votre prenom:\n");
-    scanf("%s",firstname);
-    printf("Entrez votre age:\n");
-    scanf("%s",age);
-    printf("Avez vous déjà utilisé un clavier ? 0/1\n");
-    scanf("%d",&keyboard);
-    printf("Avez vous un handicap ? 0/1\n"); 
-    printf("Lequel : Daltonien(1) Malvoyant(2) Moteur(3)\n");
-    scanf("%d",&handicap);
-   
-    profile = determine_profile(age,keyboard,handicap);
-    
-    // créer le profile
-    user = find_user(name,firstname);
-    if(user == NULL)
-        user = init_user(name,firstname,age,profile);
-   
-    return user; 
+  struct user *user;
+  struct profile *profile;
+  char name[50];
+  char firstname[50];
+  char age[3];
+  int keyboard;
+  int handicap;
+  int category;
+
+  printf("Entrez votre nom:\n");
+  scanf("%s",name);
+  printf("Entrez votre prenom:\n");
+  scanf("%s",firstname);
+  printf("Entrez votre age:\n");
+  scanf("%s",age);
+  printf("Avez vous déjà utilisé un clavier ? 0/1\n");
+  scanf("%d",&keyboard);
+
+  //TODO Query select to get all the handicap possible
+  // then loop printf ( handicapname, i)
+  printf("Avez vous un handicap ? 0/1\n");
+  printf("Lequel : Daltonien(1) Moteur(2)\n");
+  scanf("%d",&handicap);
+
+  if ((category = determine_category) == -1)
+    err("1", "error while determine category");
+
+  // créer le profile
+  profile = init_profile(category,user);
+
+  // initialize the user
+  if( find_user(name,firstname, user) == -1)
+  {
+    if ((user = init_user(name,firstname,age,profile)) == NULL)
+      err("1", "error while initialize user");
+  }
+
+  return 1;
 }
