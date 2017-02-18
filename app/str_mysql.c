551fe791309b8bd74ecdec22c067d0627e3b945b
mbvt <morgan.bouvet@epita.fr>
2017-02-18 20:00:18 +0100

    LINK BDD ISSUE : Store result set

--- app/str_mysql.c
+++ app/str_mysql.c
@@ -2,9 +2,14 @@
 
 #include "str_mysql.h"
 
-void finish_with_error(MYSQL *con){
-  fprintf(stderr, "%s\n", mysql_error(con));
-  mysql_close(con);
+void finish_with_error(MYSQL *mysql){
+  printf("Error: %s\n", mysql_error(mysql));
+  mysql_close(mysql);
+}
+
+void finish_success(MYSQL *mysql){
+  printf("Success !\n");
+  mysql_close(mysql);
 }
 
 int init_sql(MYSQL *mysql)
@@ -24,7 +29,7 @@ int connect_sql(MYSQL *mysql, char *host, char *user, char *pwd)
 
   if(mysql_real_connect(mysql, host, user, pwd, db, 0, NULL, 0) == NULL)
   {
-    printf("Failed to connect database.\nError : %s\n", mysql_error(mysql);
+    printf("Failed to connect database %s.\nError : %s\n", db, mysql_error(mysql));
     mysql_close(mysql);
     return 0;
   }
@@ -32,5 +37,98 @@ int connect_sql(MYSQL *mysql, char *host, char *user, char *pwd)
   return 1;
 }
 
+char *find_fields(S_MYSQL *smysql)
+{
+  char *str1 = "select COLUMN_NAME from INFORMATION_SCHEMA.COLUMNS";
+  char *str2 = " where table_name = \'";
+  char *end = "\';";
+
+  unsigned int len = strlen(str1) + strlen(str2) + strlen(smysql->table_name) + strlen(end);
+
+  char *query = malloc(len * sizeof(char));
+  strcat(query, str1);
+  strcat(query, str2);
+  strcat(query, smysql->table_name);
+  strcat(query, end);
+
+  if(mysql_query(smysql->con, query))
+    finish_success(smysql->con);
+
+  if(mysql_store_result(smysql->con))   //TODO mysql_use_result
+   {
+    finish_success(smysql->con);
+    printf("OK stored\n");
+   }
+  else
+  {
+    printf("NOPE\n");
+  }
+
+  MYSQL_RES *result = mysql_store_result(smysql->con);
+  unsigned int num_fields = mysql_num_rows(result);
+
+  MYSQL_FIELD *fields;
+  fields = mysql_fetch_fields(result);
+
+  char *res = "";
+  for(unsigned int i = 0; i < num_fields; ++i)
+  {
+    printf("Le champ %u est %s\n", i, fields[i].name);
+    strcat(res, fields[i].name);
+    strcat(res, ", ");
+  }
 
 
+
+ /* char *res_tab[num_rows];
+  char *res;
+
+  for(int i = 0; i < num_rows; ++i)
+  {
+    res_tab[i] = (char*)result[i];
+    printf("%s\n", res_tab[i]);
+    res = strcat(res, res_tab[i]);
+  }
+  */
+
+  return res;
+}
+
+int insert_table(S_MYSQL *smysql)
+{
+  /*char *flds = "";
+  char *vals = "";
+  if(mysql_query(smysql->con, "Insert into "+ smysql->table_name +"("+ flds +
+        ") values (" + vals + ");"))
+    finish_success(smysql->con);
+*/
+  return 0;
+}
+
+
+int main()//int argc, char* argv[])
+{
+  int check = 1;
+  S_MYSQL *req = malloc(sizeof(struct S_MYSQL));
+  MYSQL var;
+
+  req->con = &var;
+  check = init_sql(req->con);
+  if(check == 0)
+    printf("Error while init in main");
+
+  req->server = "localhost";
+  req->user = "adminPIK";
+  req->password = "projetpik";
+  req->db = "projetpik";
+  req->table_name = "pik_user";
+
+    check = connect_sql(req->con, req->server, req->user, req->password);
+  if(check == 0)                                                                
+    printf("Error while connecting db in main");
+
+  char *res = find_fields(req);
+  printf("RES MAIN : %s\n", res);
+
+  return 0;
+}
--- app/str_mysql.h
+++ app/str_mysql.h
@@ -5,26 +5,34 @@
 
 #include <mysql/mysql.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 #define MAX_NAME_LENGTH   128
 #define MAX_QUERY_LENGTH  1024
 
-typedef struct {
+typedef struct S_MYSQL{
   MYSQL *con;
-  char *password[MAX_NAME_LENGTH];
-  char *root_password[MAX_NAME_LENGTH];
-  char *user[MAX_NAME_LENGTH];
-  char *server[MAX_NAME_LENGTH];
-  char *database_name[MAX_NAME_LENGTH];
-  char *table_name[MAX_NAME_LENGTH];
-  char *query_create_table[MAX_QUERY_LENGTH];
-  char *query_create_db[MAX_QUERY_LENGTH];
-  char *query_data[MAX_QUERY_LENGTH];
+  char *password;//[MAX_NAME_LENGTH];
+  char *root_password;//[MAX_NAME_LENGTH];
+  char *user;//[MAX_NAME_LENGTH];
+  char *server;//[MAX_NAME_LENGTH];
+  char *db;//[MAX_NAME_LENGTH];
+  char *table_name;//[MAX_NAME_LENGTH];
+  char *query_create_table;//[MAX_QUERY_LENGTH];
+  char *query_create_db;//[MAX_QUERY_LENGTH];
+  char *query_data;//[MAX_QUERY_LENGTH];
 }S_MYSQL;
 
 
-void finish_with_error(MYSQL *con);
-int s_mysql_init(S_MYSQL *mysql);
+void finish_with_error(MYSQL *mysql);
+void finish_success(MYSQL *mysql);
+int init_sql(MYSQL *mysql);
+int connect_sql(MYSQL *mysql, char *host, char *user, char *pwd);
+char *find_fields(S_MYSQL *smysql);
+
+
+/*int s_mysql_init(S_MYSQL *mysql);
 int s_mysql_connect(S_MYSQL *mysql);
 int s_mysql_disconnect(S_MYSQL *mysql);
 //int s_mysql_createdb(S_MYSQL *mysql);
@@ -32,5 +40,5 @@ int s_mysql_createtable(S_MYSQL *mysql);
 int s_mysql_setdb(S_MYSQL *mysql);
 int s_mysql_insertdata(S_MYSQL *mysql);
 int s_mysql_quickinsert(S_MYSQL *mysql);
-
+*/
 #endif /* STR_MYSQL_H_ */
