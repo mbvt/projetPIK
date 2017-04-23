fb1765c56d906bc284d77cca536eba49cd16ef2d
ahmedali <amine.ahmed-ali@epita.fr>
2017-04-23 21:56:05 +0200

    Reclone

--- fusion/main.c
+++ fusion/main.c
@@ -1,16 +1,10 @@
 # include "main.h"
 
 
-struct S_GSQL *conn;
-
 /*----------------------------------MAIN---------------------------------------
  *---------------------------------------------------------------------------*/
 int main(int argc, char *argv[])
 {
-
-  //struct S_GSQL *conn = NULL;
-  //conn->smysql = connect_db(conn->smysql);
-
   gtk_init(&argc, &argv);
 
   builder = gtk_builder_new();
@@ -22,8 +16,11 @@ int main(int argc, char *argv[])
   TestInsPage  = GTK_WIDGET(gtk_builder_get_object(builder, "TestInsPage"));
 
   typed = GTK_LABEL(gtk_builder_get_object(builder, "typed"));
+  ok = GTK_LABEL(gtk_builder_get_object(builder, "ok"));
 
+  entryok = GTK_ENTRY(gtk_builder_get_object(builder, "entryok"));
   entry = GTK_ENTRY(gtk_builder_get_object(builder, "entry"));
+  entryP = GTK_ENTRY(gtk_builder_get_object(builder, "entryP"));
   g_signal_connect(entry, "activate", G_CALLBACK(on_CoEntry_clicked), entry);
 
   testCo = GTK_LABEL(gtk_builder_get_object(builder, "testCo"));
@@ -44,12 +41,35 @@ int main(int argc, char *argv[])
   Dalt = GTK_TOGGLE_BUTTON(gtk_builder_get_object(builder, "Dalt"));
   Mot  = GTK_TOGGLE_BUTTON(gtk_builder_get_object(builder, "Mot"));
 
-  g_signal_connect(UCO, "activate", G_CALLBACK(on_Game_clicked), UCO);
-  g_signal_connect(UCN, "activate", G_CALLBACK(on_Game_clicked), UCN);
-  g_signal_connect(HO, "activate", G_CALLBACK(on_Game_clicked), HO);
-  g_signal_connect(HN, "activate", G_CALLBACK(on_Game_clicked), HN);
+  g_signal_connect(UCO , "activate", G_CALLBACK(on_Game_clicked), UCO);
+  g_signal_connect(UCN , "activate", G_CALLBACK(on_Game_clicked), UCN);
+  g_signal_connect(HO  , "activate", G_CALLBACK(on_Game_clicked), HO);
+  g_signal_connect(HN  , "activate", G_CALLBACK(on_Game_clicked), HN);
   g_signal_connect(Dalt, "activate", G_CALLBACK(on_Game_clicked), Dalt);
-  g_signal_connect(Mot, "activate", G_CALLBACK(on_Game_clicked), Mot);
+  g_signal_connect(Mot , "activate", G_CALLBACK(on_Game_clicked), Mot);
+
+
+  D  = GTK_TOGGLE_BUTTON(gtk_builder_get_object(builder, "D"));
+  I  = GTK_TOGGLE_BUTTON(gtk_builder_get_object(builder, "I"));
+  E  = GTK_TOGGLE_BUTTON(gtk_builder_get_object(builder, "E"));
+  M1 = GTK_TOGGLE_BUTTON(gtk_builder_get_object(builder, "M1"));
+  M2 = GTK_TOGGLE_BUTTON(gtk_builder_get_object(builder, "M2"));
+  M3 = GTK_TOGGLE_BUTTON(gtk_builder_get_object(builder, "M3"));
+  N1 = GTK_TOGGLE_BUTTON(gtk_builder_get_object(builder, "N1"));
+  N2 = GTK_TOGGLE_BUTTON(gtk_builder_get_object(builder, "N2"));
+  N3 = GTK_TOGGLE_BUTTON(gtk_builder_get_object(builder, "N3"));
+
+
+  g_signal_connect(D , "activate", G_CALLBACK(on_validercat_clicked), D);
+  g_signal_connect(I , "activate", G_CALLBACK(on_validercat_clicked), I);
+  g_signal_connect(E , "activate", G_CALLBACK(on_validercat_clicked), E);
+  g_signal_connect(M1, "activate", G_CALLBACK(on_validercat_clicked), M1);
+  g_signal_connect(M2, "activate", G_CALLBACK(on_validercat_clicked), M2);
+  g_signal_connect(M3, "activate", G_CALLBACK(on_validercat_clicked), M3);
+  g_signal_connect(N1, "activate", G_CALLBACK(on_validercat_clicked), N1);
+  g_signal_connect(N2, "activate", G_CALLBACK(on_validercat_clicked), N2);
+  g_signal_connect(N3, "activate", G_CALLBACK(on_validercat_clicked), N3);
+
 
   g_object_unref (G_OBJECT (builder));
 
@@ -94,29 +114,30 @@ void on_qcmback_clicked()
 
 void on_CoEntry_clicked()
 {
-  const char *sText;
-  char *te = "Morgan";
 
-  sText = gtk_entry_get_text(GTK_ENTRY(entry));
+  struct S_MYSQL *smysql = NULL;
+  smysql = connect_db(smysql);
+  smysql->table_name = "pik_user";
+
+  const char *pp, *nn;
+  pp = gtk_entry_get_text(GTK_ENTRY(entry));
+  nn = gtk_entry_get_text(GTK_ENTRY(entryP));
+
+  char *f = (char *)pp;
+  char *n = (char *)nn;
 
 
-  if(*sText == *te)
+  if(exist_user(f, n, smysql))
   {
-    gtk_label_set_text(testCo, sText);
-    printf("Nom d'utilisateur saisi : %s\n", sText);
-    gtk_stack_set_visible_child_name(GTK_STACK(IHM), "TestCoPage");
+    gtk_label_set_text(testCo, f);
+    gtk_stack_set_visible_child_name(GTK_STACK(IHM), "CatePage");
   }
 
-  else
-  {
-    gtk_label_set_text(errCo, "Nom d'utilisateur non trouvé");
-  }
-
+  gtk_label_set_text(errCo, "Nom d'utilisateur non trouvé");
 }
 
-void on_Game_clicked()//struct user *user, struct S_MYSQL *smysql)
+void on_Game_clicked()
 {
-
 
   struct S_MYSQL *smysql = NULL;
   smysql = connect_db(smysql);
@@ -133,10 +154,6 @@ void on_Game_clicked()//struct user *user, struct S_MYSQL *smysql)
   const char *na = gtk_entry_get_text(GTK_ENTRY(name));
   const char *fa = gtk_entry_get_text(GTK_ENTRY(firstname));
   const char *aa = gtk_entry_get_text(GTK_ENTRY(age));
-  printf("Nom    : %s\n", na);
-  printf("Prénom : %s\n", fa);
-  printf("Age    : %s ans\n", aa);
-
 
   char *n = (char *)na;
   char *f = (char *)fa;
@@ -148,7 +165,6 @@ void on_Game_clicked()//struct user *user, struct S_MYSQL *smysql)
   {
     int id_user = 0;
     user->firstname = f;
-    printf("%s\n", user->firstname);
     user->name = n;
     user->age = a;
     user->status = status;
@@ -161,10 +177,6 @@ void on_Game_clicked()//struct user *user, struct S_MYSQL *smysql)
     else
     {
       user = get_user(user->name, user->firstname, smysql);
-      printf("User correctly inserted :\n");
-      printf("ID : %d\nFirstname : %s\n", user->id_user, user->firstname);
-      printf("Name : %s\nAge : %d\n", user->name, user->age);
-      printf("Status : %d\nCategory : %d\n", user->status, user->category);
     }
   }
   else
@@ -192,12 +204,9 @@ void on_Game_clicked()//struct user *user, struct S_MYSQL *smysql)
 
   g_signal_connect(MainWindow, "key-release-event", G_CALLBACK(key_event), NULL);
 
-  gtk_label_set_text(typed, "");
-
   gtk_entry_set_text(name, "");
   gtk_entry_set_text(firstname, "");
   gtk_entry_set_text(age, "");
-
 
   gtk_toggle_button_set_active(UCO, FALSE);
   gtk_toggle_button_set_active(UCN, FALSE);
@@ -205,6 +214,7 @@ void on_Game_clicked()//struct user *user, struct S_MYSQL *smysql)
   gtk_toggle_button_set_active(HN, FALSE);
   gtk_toggle_button_set_active(Dalt, FALSE);
   gtk_toggle_button_set_active(Mot, FALSE);
+
 }
 
 void on_gameback_clicked()
@@ -212,54 +222,96 @@ void on_gameback_clicked()
   gtk_stack_set_visible_child_name(GTK_STACK(IHM), "QcmPage");
 }
 
+const char *tmp;
 
-static gboolean *key_event(GtkWidget *widget, GdkEventKey *event)   {
-  char *dst = calloc(20, sizeof(char));
-  char *c = gdk_keyval_name(event->keyval);
-  strcat(dst, c);
-  gtk_label_set_text(typed, dst);
+static gboolean *key_event()//GtkWidget *widget, GdkEventKey *event)
+{
+  //char *c = gdk_keyval_name(event->keyval);
+  //printf("%s\n", c);
+  size_t cpt = 0;
+  const char *c = gtk_entry_get_text(GTK_ENTRY(entryok));
+  const gchar *str = gtk_label_get_text(typed);
+  tmp = gtk_entry_get_text(GTK_ENTRY(entryok));
+  size_t l = strlen(c);
+  size_t le = strlen(tmp);
+  size_t len = strlen(str);
+  char *pts = calloc(10, sizeof(char));
+  if(cpt < len)
+  {
+    for(size_t j = 0; j < le; j++)
+    {
+      if(tmp[j] != c[j])
+        cpt--;
+    }
+    for(size_t i = 0; i < l; i++)
+    {
+      if(str[i] == c[i])
+        cpt++;
+      else if(str[i] != c[i])
+        cpt--;
+    }
+  }
+
+  sprintf(pts, "%zu", cpt);
+  gtk_label_set_text(ok, pts);
   return FALSE;
 }
+void on_validercat_clicked()
+{
+  if( gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(D)) )
+    printf("Débutant\n");
+  if( gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(I)) )
+    printf("Intermédiaire \n");
+  if( gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(E)) )
+    printf("Expert \n");
+  if( gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(M1)) )
+    printf("Monde 1 \n");
+  if( gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(M2)) )
+    printf("Monde 2 \n");
+  if( gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(M3)) )
+    printf("Monde 3 \n");
+  if( gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(N1)) )
+    printf("Niveau 1 \n");
+  if( gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(N2)) )
+    printf("Niveau 2 \n");
+  if( gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(N3)) )
+    printf("Niveau 3 \n");
+
+  gtk_stack_set_visible_child_name(GTK_STACK(IHM), "GamePage");
+
+  g_signal_connect(MainWindow, "key-release-event", G_CALLBACK(key_event), NULL);
+}
+
+
+void on_cateba_clicked()
+{
+  gtk_stack_set_visible_child_name(GTK_STACK(IHM), "MainPage");
+}
+
+void on_gameback1_clicked()
+{
+  gtk_stack_set_visible_child_name(GTK_STACK(IHM), "CatePage");
+}
+
 
 
 /*-----------------------------QUIT BUTTON-------------------------------------
  *---------------------------------------------------------------------------*/
 
-void on_quit_clicked()
-{
-  gtk_main_quit();
-}
+void on_quit_clicked(){ gtk_main_quit(); }
 
-void on_coquit_clicked()
-{
-  gtk_main_quit();
-}
+void on_coquit_clicked(){ gtk_main_quit(); }
 
-void on_insquit_clicked()
-{
-  gtk_main_quit();
-}
+void on_insquit_clicked(){ gtk_main_quit(); }
 
-void on_QcmQuit_clicked()
-{
-  gtk_main_quit();
-}
+void on_QcmQuit_clicked(){ gtk_main_quit(); }
 
-void on_TCPQuit_clicked()
-{
-  gtk_main_quit();
-}
+void on_TCPQuit_clicked(){ gtk_main_quit(); }
 
-void on_MainWindow_main_destroy()
-{
-  gtk_main_quit();
-}
+void on_MainWindow_main_destroy(){ gtk_main_quit(); }
 
-void on_gameQuit_clicked()
-{
-  gtk_main_quit();
-}
+void on_gameQuit_clicked(){ gtk_main_quit(); }
 
+void on_catequi_clicked(){ gtk_main_quit(); }
 
-
-
+void on_gameQuit1_clicked(){ gtk_main_quit(); }
--- fusion/main.h
+++ fusion/main.h
@@ -5,6 +5,7 @@
 # include <SDL/SDL_image.h>
 # include "user.h"
 
+
 extern struct S_GSQL {
   struct S_MYSQL *smysql;
   struct user *user;
@@ -13,12 +14,12 @@ extern struct S_GSQL {
 
 GtkWidget  *Connexion, *Inscription, *MainWindow, *TestInsPage;
 GtkWidget  *CoWindow, *InsWindow, *IHM, *CoEntry;
-GtkEntry   *entry, *name, *firstname, *age;
-GtkToggleButton *UCO, *UCN, *HO, *HN, *Dalt, *Mot;
+GtkEntry   *entry, *entryP, *entryok, *name, *firstname, *age;
 GtkBuilder *builder;
-GtkLabel   *testCo, *errCo;
-GtkLabel *typed;
-
+GtkLabel   *testCo, *errCo, *typed, *ok;
+GtkToggleButton *UCO, *UCN, *HO, *HN, *Dalt, *Mot;
+GtkToggleButton *D, *I, *E, *M1, *M2, *M3, *N1, *N2, *N3;
+GSList *group1, *group2, *group3;
 
 void on_Connexion_clicked();
 void on_Inscription_clicked();
@@ -26,5 +27,6 @@ void on_connback_clicked();
 void on_insback_clicked();
 void on_qcm_clicked();
 void on_CoEntry_clicked();
-void on_Game_clicked();//struct user *user, struct S_MYSQL *smysql);
-static gboolean *key_event(GtkWidget *widget, GdkEventKey *event);
+void on_Game_clicked();
+void on_validercat_clicked();
+static gboolean *key_event();//GtkWidget *widget, GdkEventKey *event);
