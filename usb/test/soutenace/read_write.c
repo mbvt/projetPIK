56f75be626b359090340f5ba0875bc5480f03a9b
Vincent Grateau <vincent.grateau@epita.fr>
2017-04-25 14:54:28 +0200

    soutenance num 2

--- usb/test/soutenace/read_write.c
+++ usb/test/soutenace/read_write.c
@@ -4,9 +4,7 @@
 #include <errno.h>
 #include <inttypes.h>
 #include <libusb-1.0/libusb.h>
-
-
-static struct libusb_device_handle *devh = NULL;
+#include <time.h>
 
 struct struct_key   {
   uint16_t   num_w;
@@ -27,48 +25,10 @@ struct struct_write {
 };
 
 
-//____________________ R E A D ______________________________________
-struct struct_key  read_chars(unsigned char * data, int size, struct struct_key key)
-{
-  int actual_length = 0;
-  int rc;
-
-  for (int i=0;i<size;data[i++]=0);;
-
-  rc = libusb_bulk_transfer(devh, 0x81, data, 8, &actual_length,
-      10);
-  if(actual_length > 0)
-  {
-    for(int i = 0; i < actual_length; i++)
-    {
-      if(data[i] != 0)
-      {
-        //key.code_key = data[i];
-        //key.position_bit = i;
-        //fprintf(stdout, " code_key      : 0x%02x \n", key.code_key);
-        //fprintf(stdout, " position_bit  : %d \n\n", key.position_bit);
-      }
-    }
-    fprintf(stdout, "----------------------  \n");
-
-  }
-  (void)rc;
-  return key;
-}
-
 //________________________C O L O R__________________________
 void keybordcolor(struct struct_write *str)
 {
   int rc = 0;
-  /*
-  fprintf(stdout, "\n\n=======color recive =====\n"); 
-
-  fprintf(stdout,"key : %02x \n", str->key);
-  fprintf(stdout,"red : %02x \n", str->red);
-  fprintf(stdout,"green : %02x \n", str->green);
-  fprintf(stdout,"blue : %02x \n", str->blue);
-  fprintf(stdout,"speed : %02x \n", str->speed);
-*/
   uint16_t speed = str->speed;
   uint16_t red = str->red;
   uint16_t green = str->green;
@@ -102,11 +62,16 @@ void keybordcolor(struct struct_write *str)
   };
 
 
+  /*rc = libusb_control_transfer(devh, 0x21, 0x09, 0x0211, 1, 
+      data_commit, sizeof(data_commit), 10); 
+  */
   rc = libusb_control_transfer(devh, 0x21, 0X09, 0x0212, 1, 
       data1, sizeof(data1), 10); 
 
   rc = libusb_control_transfer(devh, 0x21, 0x09, 0x0211, 1, 
       data_commit, sizeof(data_commit), 10); 
+  
+  
   (void)rc;
 }
 
@@ -132,8 +97,47 @@ libusb_device_handle* USB_Init()
 }
 
 //_____________________ U S B  -  C L O S E  ________________________
-void USB_Close(libusb_device_handle *devh)
+void USB_Close(libusb_device_handle *devh, int i)
 {
+  if (i == 1)
+  {
+    int rc = 0;
+  unsigned char data_commit [] = { 0x11, 0xff, 0x0c, 0x5a, 
+    0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00
+    };
+  
+  unsigned char wave [] =  {0x11, 0xff, 0x0d, 0x3c, 0xFF, 0x04,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x88, 0x01, 0x64, 0x01, 0x00, 0x00,
+    0x00, 0x00 };
+
+   unsigned char reseat [] = {0x11, 0xff, 0x0d, 0x3c, 0xff, 0x01, 0x00, 0x00,
+   0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+   /*
+   rc = libusb_control_transfer(devh, 0x21, 0X09, 0x0211, 1,                     
+                                  wave, sizeof(wave), 100);
+printf("rc = %d \n", rc);  
+  rc = libusb_control_transfer(devh, 0x21, 0x09, 0x0211, 1, 
+      data_commit, sizeof(data_commit), 10); 
+
+printf("rc = %d \n", rc);  
+*/
+  rc = libusb_control_transfer(devh, 0x21, 0x09, 0x0211, 1, 
+      data_commit, sizeof(data_commit), 10); 
+printf("rc = %d \n", rc);  
+  
+  rc = libusb_control_transfer(devh, 0x21, 0X09, 0x0211, 1, reseat,
+    sizeof(reseat), 10);
+printf("rc = %d \n", rc);  
+  
+  rc = libusb_control_transfer(devh, 0x21, 0x09, 0x0211, 1, 
+      data_commit, sizeof(data_commit), 10); 
+printf("rc = %d \n", rc);  
+
+(void)rc;
+  }
   libusb_release_interface(devh, 0);
   libusb_release_interface(devh, 1);
 
@@ -143,9 +147,9 @@ void USB_Close(libusb_device_handle *devh)
   if(devh)
     libusb_close(devh);
   libusb_exit(NULL);
-}
+} 
 
-
+//____________ WRITE_TO_KEYBORD_______________
 int write_to_keybord (libusb_device_handle *devh)
 {
   struct struct_write *str = malloc (sizeof (struct struct_write ));  
@@ -160,8 +164,8 @@ int write_to_keybord (libusb_device_handle *devh)
   rc = scanf("%04x" , &str->green);
   fprintf(stdout,"\n Blue :");
   rc = scanf("%04x" , &str->blue);
-  fprintf(stdout,"\n Blue :");
   fprintf(stdout,"\n");
+
   str->speed = 0x01;
   str->devh = devh;
   str->key = variable_local;
@@ -171,37 +175,78 @@ int write_to_keybord (libusb_device_handle *devh)
   free(str);    
   (void)rc;
   
-  if(str->key == 0x29)
+  if(variable_local == 0x29)
     return 1;
 
   return 0;
 }
-
+ 
 
 //_______________ R E A D - T O - K E Y B O R D _________________
-int   read_to_keybord(libusb_device_handle *devh)
+struct struct_write*   read_to_keybord(libusb_device_handle *devh)
 {
-  unsigned char data_read[64];
-  int size = 64;
+  unsigned char data_read[1024];
+  int size = 1024;
   int rc = 0;
   int actual_length = 0;
+  int flag = 0;
+  struct struct_write *str = malloc (sizeof (struct struct_write ));  
 
   for ( int i=0 ; i < size ; data_read[i++] = 0 );; 
 
-  rc = libusb_bulk_transfer(devh, 0x81, data_read, 8, &actual_length, 10);
+  rc = libusb_bulk_transfer(devh, 0x81, data_read, 1024, &actual_length, 1);
+  
   if(actual_length > 0)
   {
     for(int i = 0; i < actual_length; i ++)
     {
       if(data_read[i] != 0) 
       {
-        fprintf(stdout, "------\nkey code : %02x \n------\n", data_read[i]);
-        if(data_read[i] == 0x29)
-          return 1;
+        str->key = data_read[i];
+        flag = 1;
+        fprintf(stdout,"\nKey ===> %02x \n", str->key);
       }
     }
   }
+  if (flag == 0)
+    str->key = 0;
   (void)rc;
+  return str;
+}
+
+int read_and_write(libusb_device_handle *devh)
+{
+  
+  uint16_t data_color [16] = {0x00, 0x10, 0x20, 0x30, 0x40, 0x50, 0x60, 0x70,
+  0x80, 0x90, 0xA0, 0xB0, 0xC0, 0xD0, 0xE0, 0xFF}; 
+  
+  struct struct_write *str = malloc (sizeof (struct struct_write ));  
+ str->key = 0; 
+  while(str->key == 0)
+  {
+    str = read_to_keybord(devh);
+  }
+  fprintf(stdout,"\nKey %02x \n", str->key);
+  str->red    = data_color[rand()%15];
+  fprintf(stdout,"Red %02x \n", str->red);
+  str->green  = data_color[rand()%15];
+  fprintf(stdout,"Green %02x \n", str->green);
+  str->blue  = data_color[rand()%15];
+  fprintf(stdout,"Blue %02x \n", str->blue);
+  
+  str->speed = 0x01;
+  str->devh = devh;
+  //str->key = 0x04;
+  
+  if(str->key != 0x00)
+      keybordcolor(str);
+
+  if(str->key == 0x29)
+  {
+    free(str);
+    return 1;
+  }
+  free(str);
   return 0;
 }
 
@@ -210,12 +255,12 @@ int main()
 {
   //INIT LIBUSB
   libusb_device_handle  *devh = NULL;
-  devh = USB_Init();
+  //devh = USB_Init();
   //-----------------------------------------|
   // TEST WITH WRITE 
   printf("begin test write : choose key to color");
   int res = 0;
-  while(res == 0)
+  /*while(res == 0)
   {
     res = write_to_keybord(devh);
   }
@@ -228,9 +273,24 @@ int main()
     res = read_to_keybord(devh);
   }
   (void)res;
+  */
+  //-----------------------------------------|
+  // TEST_TO_READ_AND_WRITE
+   //res = 0;
+     // devh = USB_Init();
+   while(res == 0)
+   {
+      devh = USB_Init();
+      res = read_and_write(devh);
+      USB_Close(devh, 0);
+   }
+   //   USB_Close(devh);
   //-------------------------------------------|
   //END TEST ==> CLOSE LIBUSB
-  USB_Close(devh);
+    printf("ici init\n");
+    devh = USB_Init();
+    printf("ici end \n");
+    USB_Close(devh, 1);
   return 0;
 } 
 //------------------- E N D _ M A I N -----------------------
