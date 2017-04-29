c1650ed56741a7d164908e75d7b29c9ad13003a0
ahmedali <amine.ahmed-ali@epita.fr>
2017-04-29 16:42:44 +0200

    separate main.c

--- fusion/main.h
+++ fusion/main.h
@@ -1,3 +1,6 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
 # include <stdlib.h>
 # include <stdio.h>
 # include <gtk/gtk.h>
@@ -7,12 +10,15 @@
 # include <SDL/SDL_image.h>
 # include "user.h"
 
-
 extern struct S_GSQL {
   struct S_MYSQL *smysql;
   struct user *user;
 } *conn;
 
+//int cpt = 0;
+//int score = 0;
+char *tmp;
+struct timespec t0,t1;
 
 GtkWidget  *Connexion, *Inscription, *MainWindow, *TestInsPage;
 GtkWidget  *CoWindow, *InsWindow, *IHM, *CoEntry;
@@ -32,5 +38,8 @@ void on_qcm_clicked();
 void on_CoEntry_clicked();
 void on_Game_clicked();
 void on_validercat_clicked();
+int compare(char *s1, char *s2);
 static gboolean *key_event_Game(GtkWidget *widget, GdkEventKey *event);
 static gboolean *key_event_Ins(GtkWidget *widget, GdkEventKey *event);
+
+#endif
